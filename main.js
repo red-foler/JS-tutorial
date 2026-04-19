1418455f4fa30cef78ad5202fd709545eb3f3106
red-foler <qwertytr20905@gmail.com>
2026-04-19 21:15:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -74,6 +74,21 @@
 //     }
 // }
 // console.log(getPrice('BMW'))
+//🟡🟡
+// const orangePrice = 3;
+// let amount = +prompt('сколько апельсинов хотите купить?');
+// let code = prompt('есть ли у вас промокод?');
+// function calculatePrice(amount, code){
+//     switch (code){
+//         case 'SAVE10':
+//             return orangePrice * amount * 0.9 + '$'
+//         case 'FREE':
+//             return "it's free for you today!"
+//     default: 
+//         return orangePrice * amount + '$'
+// }};
+
+// alert(calculatePrice(amount, code));
 
 
 
